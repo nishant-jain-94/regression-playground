83dd21a4536b2a131f7c074b2a2498ea9cdda3e8
Nishant Jain <nishantkumarajain@gmail.com>
2016-12-09 15:00:49 +0530

    Replaced nsteps with tolerance of 0.01

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn import linear_model
 
 def generating_data(n, m=2.25, b=6.0, stddev=1.5):
     x = np.linspace(-2.0, 2.0, n)
@@ -34,18 +35,33 @@ def gradient_descent(X, Y, nsteps, step_size=0.0001):
     """
 
     m, c = 0, 0
-    yield m, c, compute_cost(X, Y, m, c)
-    # print(m, c, compute_cost(X, Y, m, c))
-    for i in range(nsteps):
+    prev_compute_cost = 0
+    current_compute_cost = compute_cost(X, Y, m, c) 
+    # yield m, c, current_compute_cost
+    # print(prev_compute_cost, current_compute_cost)
+    # print(abs(prev_compute_cost - current_compute_cost))
+    while(abs(prev_compute_cost - current_compute_cost) > 0.01):
+        # print("Inside while loop")
         Yhat = m * X + c
         diff = Yhat - Y
         dm = step_size * (diff * X).sum() * 2 / X.shape[0]
         dc = step_size *  diff.sum() * 2 / X.shape[0]
         m -= dm
         c -= dc
-        # print(m, c, compute_cost(X, Y, m, c))
-        yield m, c, compute_cost(X, Y, m, c)
+        prev_compute_cost, current_compute_cost = current_compute_cost, compute_cost(X, Y, m, c)
+        # yield m, c, current_compute_cost
+    return m, c, current_compute_cost
 
-X, Y = pd.Series([95, 85, 80, 70, 60]), pd.Series([85, 95, 70, 65, 70])
-for i in iter(gradient_descent(X, Y, 1000)):
-    print(i)
+def get_regression_coeffiecients(X, Y, nsteps, step_size=0.0001):
+    """Returns the regression coefficents obtained through scikit learn. """
+    regr = linear_model.LinearRegression(fit_intercept=True)
+    regr.fit(X, Y)
+    print(regr.get_params(deep=True))  
+    print("Coefficients", regr.coef_)
+
+
+sales = pd.read_csv("Philadelphia_Crime_Rate_noNA.csv")
+X, Y = sales['CrimeRate'], sales['HousePrice']
+# X, Y = pd.Series([95, 85, 80, 70, 60]), pd.Series([85, 95, 70, 65, 70])
+print(gradient_descent(X, Y, 100000))
+get_regression_coeffiecients(np.transpose(np.matrix(X)), np.transpose(np.matrix(Y)), 1000)
