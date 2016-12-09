Fixed the gradient descent and the cost

simple_linear_regression.py
@@ -1,8 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def generating_data(n, m=2.25, b=6.0, stddev=1.5):
     x = np.linspace(-2.0, 2.0, n)
-    y = x * m + b
+    y = x * m + b + np.random.normal(loc=0, scale=stddev, size=n)
     return x, y
 
 def compute_cost(X, Y, m, c):
@@ -18,11 +20,11 @@ def compute_cost(X, Y, m, c):
     
     Yhat = m * X + c
     diff = Yhat - Y
-    residual_sum_of_squares = np.dot(diff.T, diff).sum()
-    return residual_sum_of_squares
+    residual_sum_of_squares = np.dot(diff.T, diff) / X.shape[0]
+    return residual_sum_of_squares.flat[0]
 
 
-def gradient_descent(X, Y, nsteps, step_size=0.1):
+def gradient_descent(X, Y, nsteps, step_size=0.0001):
     """Returns the minimal value of the parameters m, c after running for nsteps. 
     
     Input:
@@ -32,15 +34,18 @@ def gradient_descent(X, Y, nsteps, step_size=0.1):
     """
 
     m, c = 0, 0
-    yield m, b, compute_cost(X, Y, m, c)
+    yield m, c, compute_cost(X, Y, m, c)
+    # print(m, c, compute_cost(X, Y, m, c))
     for i in range(nsteps):
         Yhat = m * X + c
         diff = Yhat - Y
-        dm = step_size * (2 * diff) * X
-        dc = step_size * (2 * diff)
+        dm = step_size * (diff * X).sum() * 2 / X.shape[0]
+        dc = step_size *  diff.sum() * 2 / X.shape[0]
         m -= dm
         c -= dc
+        # print(m, c, compute_cost(X, Y, m, c))
         yield m, c, compute_cost(X, Y, m, c)
 
-
-generating_data(50)
+X, Y = pd.Series([95, 85, 80, 70, 60]), pd.Series([85, 95, 70, 65, 70])
+for i in iter(gradient_descent(X, Y, 1000)):
+    print(i)